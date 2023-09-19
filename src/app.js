c43e2932ffc8a27ac9f96e8aaf4efda3cce7d470
andrey100f <andreiforminte19@yahoo.com>
2023-09-19 21:14:20 +0300

    Fixed mail contact

--- src/app.js
+++ src/app.js
@@ -6,3 +6,22 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
+
+function sendMessage() {
+    var serviceId = "service_hywezml";
+    var templateId = "template_ccr6gic";
+
+    var params = {
+        senderName: document.querySelector('#name').value,
+        senderEmail: document.querySelector('#email').value,
+        comment: document.querySelector('#comment').value
+    };
+
+    emailjs.send(serviceId, templateId, params)
+        .then(res => {
+            alert('Your message has been sent!!');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
