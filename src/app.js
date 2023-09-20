99fa3058300ca52e074f34b1f8d46cb7032c4af8
andrey100f <andreiforminte19@yahoo.com>
2023-09-20 06:34:45 +0300

    Test form

--- src/app.js
+++ src/app.js
@@ -4,30 +4,30 @@ function toggleButton() {
     navList.classList.toggle('show');
 }
 
-// const form = document.querySelector('#contactForm');
+const form = document.querySelector('#contactForm');
 
-// form.addEventListener('submit', (e) => {
-//     const captchaResponse = grecaptcha.getResponse();
-//     console.log(captchaResponse);
+form.addEventListener('submit', (e) => {
+    const captchaResponse = grecaptcha.getResponse();
+    console.log(captchaResponse);
 
-//     if (!captchaResponse.length > 0) {
-//         throw new Error("Captcha not complete!!");
-//     }
+    if (!captchaResponse.length > 0) {
+        throw new Error("Captcha not complete!!");
+    }
 
-//     var serviceId = "service_hywezml";
-//     var templateId = "template_ccr6gic";
+    var serviceId = "service_hywezml";
+    var templateId = "template_ccr6gic";
 
-//     var params = {
-//         senderName: document.querySelector('#name').value,
-//         senderEmail: document.querySelector('#email').value,
-//         comment: document.querySelector('#comment').value
-//     };
+    var params = {
+        senderName: document.querySelector('#name').value,
+        senderEmail: document.querySelector('#email').value,
+        comment: document.querySelector('#comment').value
+    };
 
-//     emailjs.send(serviceId, templateId, params)
-//         .then(res => {
-//             alert('Your message has been sent!!');
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         });
-// })
+    emailjs.send(serviceId, templateId, params)
+        .then(res => {
+            alert('Your message has been sent!!');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
