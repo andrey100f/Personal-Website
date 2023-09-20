448e01f94816c040aba0c124e9cc2a518f1b81cb
andrey100f <andreiforminte19@yahoo.com>
2023-09-20 08:32:54 +0300

    Fixed the contact form

--- src/app.js
+++ src/app.js
@@ -8,16 +8,8 @@ const form = document.querySelector('#contactForm');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const senderName = document.querySelector('#name').value;
-    const senderEmail = document.querySelector('#email').value;
-    const comment = document.querySelector('#comment').value;
-
-    console.log(senderName);
-    console.log(senderEmail);
-    console.log(comment);
 
     const captchaResponse = grecaptcha.getResponse();
-    console.log(captchaResponse);
 
     if (captchaResponse.length == 0) {
         e.preventDefault();
